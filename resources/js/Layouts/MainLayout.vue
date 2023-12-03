06cb6c588c816b83e464dab09c19d28d36f39d26
Minkhantnaung@securelink <minkhantnaung@securelinkmm.com>
2023-12-03 22:03:49 +0630

    MainLayout-top-nav

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -14,11 +14,40 @@ import AccountPlusOutline from 'vue-material-design-icons/AccountPlusOutline.vue
 </script>
 
 <template>
-    <div id="main-layout" class="bg-red-600">
-        <div>
-            MainLayout
+    <div id="main-layout" class="w-full h-screen">
+        <div v-if="$page.url === '/'" id="top-nav-home"
+            class="fixed z-38 md:hidden block w-full bg-white h-[61px] border-b border-b-gray-300">
+            <div class="flex items-center justify-between h-full">
+                <Link href="/">
+                <img src="/insta-logo.png" alt="" class="w-[105px] ml-6 cursor-pointer">
+                </Link>
+
+                <div class="flex items-center w-[50%]">
+                    <div class="flex items-center w-full bg-gray-100 rounded-lg">
+                        <Magnify class="pl-4" fillColor="#8E8E8E" :size="27" />
+                        <input type="text" placeholder="Search" class="bg-transparent w-full placeholder-[#8E8E8E] border-0
+                            ring-0 focus:ring-0">
+                    </div>
+
+                    <HeartOutline class="pl-4 pr-3" fillColor="#000000" :size="27" />
+                </div>
+            </div>
         </div>
 
-        <slot />
+        <div v-if="$page.url !== '/'" id="top-nav-user"
+            class="md:hidden fixed flex items-center justify-between z-30 w-full bg-white h-[61px] border-b border-b-gray-300">
+            <Link href="/" class="px-4">
+                <ChevronLeft :size="30" class="cursor-pointer" />
+                <div class="font-extrabold text-lg">NAME HERE</div>
+                <AccountPlusOutline :size="30" class="cursor-pointer px-4" />
+            </Link>
+        </div>
+
+        <div id="side-nav" class="fixed h-full bg-white xl:w-[280px] w-[80px] md:block hidden border-r border-r-gray-300">
+            <Link href="/">
+                <img src="/insta-logo-small.png" alt="" class="xl:hidden block w-[25px] mt-10 ml-[25px] mb-10 cursor-pointer">
+                <img src="/insta-logo.png" alt="" class="xl:block hidden w-[120px] mt-10 ml-6 mb-10 cursor-pointer">
+            </Link>
+        </div>
     </div>
 </template>
