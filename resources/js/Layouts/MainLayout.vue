866aacf2b45f01f9cc600ac7809ffcaa7dff8179
Minkhantnaung@securelink <minkhantnaung@securelinkmm.com>
2023-12-07 21:55:47 +0630

    MenuItem component

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -11,10 +11,14 @@ import Plus from 'vue-material-design-icons/Plus.vue'
 import AccountOutline from 'vue-material-design-icons/AccountOutline.vue'
 import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue'
 import AccountPlusOutline from 'vue-material-design-icons/AccountPlusOutline.vue'
+
+import MenuItem from '@/Components/MenuItem.vue'
 </script>
 
 <template>
     <div id="main-layout" class="w-full h-screen">
+
+        <!-- Top Nav START -->
         <div v-if="$page.url === '/'" id="top-nav-home"
             class="fixed z-38 md:hidden block w-full bg-white h-[61px] border-b border-b-gray-300">
             <div class="flex items-center justify-between h-full">
@@ -37,17 +41,42 @@ import AccountPlusOutline from 'vue-material-design-icons/AccountPlusOutline.vue
         <div v-if="$page.url !== '/'" id="top-nav-user"
             class="md:hidden fixed flex items-center justify-between z-30 w-full bg-white h-[61px] border-b border-b-gray-300">
             <Link href="/" class="px-4">
-                <ChevronLeft :size="30" class="cursor-pointer" />
-                <div class="font-extrabold text-lg">NAME HERE</div>
-                <AccountPlusOutline :size="30" class="cursor-pointer px-4" />
+            <ChevronLeft :size="30" class="cursor-pointer" />
+            <div class="font-extrabold text-lg">NAME HERE</div>
+            <AccountPlusOutline :size="30" class="cursor-pointer px-4" />
             </Link>
         </div>
+        <!-- Top Nav END -->
 
+        <!-- Side Nav START -->
         <div id="side-nav" class="fixed h-full bg-white xl:w-[280px] w-[80px] md:block hidden border-r border-r-gray-300">
             <Link href="/">
-                <img src="/insta-logo-small.png" alt="" class="xl:hidden block w-[25px] mt-10 ml-[25px] mb-10 cursor-pointer">
-                <img src="/insta-logo.png" alt="" class="xl:block hidden w-[120px] mt-10 ml-6 mb-10 cursor-pointer">
+            <img src="/insta-logo-small.png" alt="" class="xl:hidden block w-[25px] mt-10 ml-[25px] mb-10 cursor-pointer">
+            <img src="/insta-logo.png" alt="" class="xl:block hidden w-[120px] mt-10 ml-6 mb-10 cursor-pointer">
+            </Link>
+
+            <div class="px-3">
+                <Link href="/">
+                <MenuItem iconString="Home" class="mb-4">
+                </MenuItem>
+                </Link>
+                <MenuItem iconString="Search" class="mb-4">
+                </MenuItem>
+                <MenuItem iconString="Explore" class="mb-4">
+                </MenuItem>
+                <MenuItem iconString="Messages" class="mb-4">
+                </MenuItem>
+                <MenuItem iconString="Notifications" class="mb-4">
+                </MenuItem>
+                <MenuItem iconString="Profile" class="mb-4">
+                </MenuItem>
+            </div>
+
+            <Link href="/" class="absolute bottom-0 px-3 w-full">
+            <MenuItem iconString="Log out" class="mb-4">
+            </MenuItem>
             </Link>
         </div>
+        <!-- Side Nav END -->
     </div>
 </template>
