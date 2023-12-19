4d27a0f7a6fbe9736f31869fbf0879f9f9841569
Minkhantnaung@securelink <minkhantnaungkzw@gmail.com>
2023-12-19 22:06:42 +0630

    add PostsResource

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\User;
+use Inertia\Inertia;
+use Illuminate\Http\Request;
+use App\Http\Resources\PostsResource;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::orderBy('id', 'desc')
+            ->get();
+
+        return Inertia::render('Home', [
+            'posts' => new PostsResource($posts),
+            'all_users' => User::all()
+        ]);
+    }
+}
---
+++ app/Http/Resources/PostsResource.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostsResource extends JsonResource
+{
+    public function toArray($request)
+    {
+        return $this->collection->map(function ($post) {
+            return [
+                'id' => $post->id,
+                'text' => $post->text,
+                'file' => $post->file,
+                'created_at' => $post->created_at->format(' M D Y'),
+                'comments' => $post->comments->map(function ($comment) {
+                    return [
+                        'id' => $comment->id,
+                        'text' => $comment->text,
+                        'user' => [
+                            'id' => $comment->user->id,
+                            'name' => $comment->user->name,
+                            'file' => $comment->user->file,
+                        ]
+                    ];
+                }),
+                'likes' => $post->likes->map(function ($like) {
+                    return [
+                        'id' => $like->id,
+                        'user_id' => $like->user_id,
+                        'post_id' => $like->post_id,
+                    ];
+                }),
+                'user' => [
+                    'id' => $post->user->id,
+                    'name' => $post->user->name,
+                    'file' => $post->user->file,
+                ]
+            ];
+        });
+    }
+}
